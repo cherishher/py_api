373d92b3025fc88ccd4d71c4073c1410cd9d5441
qinshimeng <1538160868@qq.com>
2015-08-24 10:04:03 +0800

    zhuxuejin list is ok

--- mod/zhu_xue_list.py
+++ mod/zhu_xue_list.py
@@ -20,8 +20,8 @@ class zhu_listHandler(tornado.web.RequestHandler):
 
         login_url = 'http://my.seu.edu.cn/userPasswordValidate.portal'
         index_url = 'http://my.seu.edu.cn/index.portal'
-        main_get_url = "http://xg.urp.seu.edu.cn/epstar/app/template.jsp?mainobj=SWMS/JXJSZ/T_JXJ_JXJZLB&tfile=XGMRMB/BGTAG_APPLY&filter=T_JXJ_JXJZLB:KTZT=1%20and%20SFKSQ=1%20and%20TO_CHAR(SYSDATE,%20'yyyy-MM-dd')%20between%20%20TO_CHAR(SQKSRQ,%20'yyyy-MM-dd')%20and%20TO_CHAR(SQJSRQ,%20'yyyy-MM-dd')%20and%20JXJZLBM%20in%20(select%20jxjzlbm%20from%20t_jxj_jxjzlb%20d%20where%20(select%20sum(a.slxd)%20from%20t_pub_e_detailnumlim%20a,t_jxj_jxjdjb%20b,t_xsjbxx_xsjbb%20c%20where%20a.slxdqbm=b.slxdqbm%20and%20b.jxjzlbm=d.jxjzlbm%20and%20a.jd=c.yxsh%20and%20c.xh='213131592'%20%20AND%20A.SLXD<>'-1'%20group%20by%20b.jxjzlbm)>0)%20OR%20jxjzlbm%20IN(SELECT%20f.jxjzlbm%20FROM%20T_JXJ_JXJDJB%20f%20WHERE%20f.jxjzlbm=jxjzlbm%20AND%20f.slxdqbm%20IS%20NULL%20AND%20KTZT%20=%201%20AND%20SFKSQ%20=%201%20AND%20TO_CHAR(SYSDATE,%20'yyyy-MM-dd')%20BETWEEN%20TO_CHAR(SQKSRQ,%20'yyyy-MM-dd')%20AND%20TO_CHAR(SQJSRQ,%20'yyyy-MM-dd'))"
-        main_get_url = "http://mynew.seu.edu.cn/alone.portal?.pen=sw.qgzx&.ia=false&.pmn=view"
+        main_get_url = "http://xg.urp.seu.edu.cn/epstar/app/template.jsp?mainobj=SWMS/ZXJGLZXT/ZXJSZ/T_ZXJ_ZL&tfile=XGMRMB/BGTAG_APPLY&filter=T_ZXJ_ZL:KTZT=1%20and%20SFKSQ=1%20and%20(SQJSSJ%20is%20not%20null%20and%20%272015-08-24%27%3C=to_char(SQJSSJ,%27yyyy-mm-dd%27)%20and%20%272015-08-24%27%20%3E=%20to_char(SQKSSJ,%20%27yyyy-mm-dd%27))%20and%20(ZXJZLBM%20in%20(select%20ZXJZLBM%20from%20t_zxj_zl%20d%20where%20(select%20sum(a.slxd)%20from%20t_pub_e_detailnumlim%20a,t_zxj_dj%20b,t_xsjbxx_xsjbb%20c%20where%20(a.slxdqbm=b.slxdqbm%20or%20b.slxdqbm%20is%20null)%20and%20b.zxjzlbm=d.zxjzlbm%20and%20a.jd=c.yxsh%20and%20c.xh=%27213131150%27%20group%20by%20b.zxjzlbm)%3E0)%20or%20zxjzlbm%20in%20(select%20zxjzlbm%20from%20t_zxj_zl%20x,%20t_pub_filter_result%20y%20where%20(x.zgxdqbh%20=%20y.filterid%20or%20x.zgxdqbh%20is%20null)%20and%20y.xh%20=%20%27213131150%27))"
+        # main_get_url = "http://mynew.seu.edu.cn/alone.portal?.pen=sw.qgzx&.ia=false&.pmn=view"
         retjson = {'code':200, 'content':''}
         if not user or not password:
             retjson['code'] = 402
@@ -51,7 +51,7 @@ class zhu_listHandler(tornado.web.RequestHandler):
                                         main_get_url,
                                         method = 'GET',
                                         headers={'Cookie':login_cookie,
-                                                 'Referer':'http://xg.urp.seu.edu.cn/epstar/app/template.jsp?mainobj=SWMS/JXJGLZXT/JXJSQ/T_JXJ_JXJXXB&tfile=XGMRMB/KJ_APPLY'},
+                                                 'Referer':' http://xg.urp.seu.edu.cn/epstar/app/template.jsp?mainobj=SWMS/ZXJGLZXT/ZXJSQ/T_ZXJ_XX&tfile=XGMRMB/KJ_APPLY'},
                                         request_timeout=7)
                     response = yield client.fetch(request)
                     retjson['content'] = self.deal(response.body)
@@ -68,19 +68,20 @@ class zhu_listHandler(tornado.web.RequestHandler):
         soup = BeautifulSoup(content)
         item = soup.findAll('td',{'nowrap':'true'})
         count = len(item)
-        all_item = (count)/10
-        if count<10:
+        all_item = (count)/11
+        if count<11:
             return ''
         else:
             ret_content = []
             for i in range(all_item):
                 temp = {
-                    'id':item[1+10*i]['pkvalue'],
-                    'name':item[2+10*i].text,
-                    'start_time':item[4+10*i].text,
-                    'end_time':item[5+10*i].text,
-                    'term':item[1+10*i]['pjxnvalue'],
-                    'type':item[6+10*i].text
+                    'name':item[2+11*i].text,
+                    'create_time':item[4+11*i].text,
+                    'is_ok':item[5+11*i].text,
+                    'pingdingzhouqi':item[6+11*i].text,
+                    'fafangzhouqi':item[7+11*i].text,
+                    'is_xiaowaishen':item[8+11*i].text,
+                    'is_jijinhui':item[9+11*i].text
                 }
                 ret_content.append(temp)
             return ret_content
